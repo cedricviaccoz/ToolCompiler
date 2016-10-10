80b0bfa4e9b2f8797d5d35bf4a85ff7a75cb5af6
cedricviaccoz <cedric.viaccoz@gmail.com>
2016-10-10 22:47:17 +0200

    Initial commit with prob lot o bug

--- src/main/scala/toolc/lexer/Lexer.scala
+++ src/main/scala/toolc/lexer/Lexer.scala
@@ -4,6 +4,7 @@ package lexer
 import utils._
 import scala.io.Source
 import java.io.File
+import com.sun.org.apache.xalan.internal.xsltc.compiler.Whitespace
 
 object Lexer extends Pipeline[File, Iterator[Token]] {
   import Tokens._
@@ -12,9 +13,68 @@ object Lexer extends Pipeline[File, Iterator[Token]] {
     * or None if it corresponds to no keyword
     */
   private def keywords(s: String): Option[Token] = s match {
-    case "program"  => Some(PROGRAM())
-    case _          => ??? // TODO
+    case ":" => Some(COLON())
+    case ";" => Some(SEMICOLON())
+    case "." => Some(DOT())
+    case "," => Some(COMMA())
+    case "=" => Some(EQSIGN())
+    case "=="=> Some(EQUALS())
+    case "!" => Some(BANG())
+    case "(" => Some(LPAREN())
+    case ")" => Some(RPAREN())
+    case "[" => Some(LBRACKET())
+    case "]" => Some(RBRACKET())
+    case "{" => Some(LBRACE())
+    case "}" => Some(RBRACE())
+    case "&&" => Some(AND())
+    case "||" => Some(OR())
+    case "<" => Some(LESSTHAN())
+    case "+" => Some(PLUS())
+    case "*" => Some(TIMES())
+    case "/" => Some(DIV())
+    case "program" => Some(PROGRAM())
+    case "class" => Some(CLASS())
+    case "def" => Some(DEF())
+    case "var" => Some(VAR())
+    case "String" => Some(STRING())
+    case "extends" => Some(EXTENDS())
+    case "Int" => Some(INT())
+    case "Bool" => Some(BOOLEAN())
+    case "if" => Some(IF())
+    case "else" => Some(ELSE())
+    case "while" => Some(WHILE())
+    case "return" => Some(RETURN())
+    case "length" => Some(LENGTH())
+    case "true" => Some(TRUE())
+    case "false" => Some(FALSE())
+    case "this" => Some(THIS())
+    case "new" => Some(NEW())
+    case "println" => Some(PRINTLN())
+    case "do" => Some(DO())
+    case _          => None
   }
+  
+  private def isASpecialChar(c: Char): Boolean = c match{
+    case ':' => true
+    case ';' => true
+    case '.' => true
+    case ',' => true
+    case '=' => true
+    case '!' => true
+    case '(' => true
+    case ')' => true
+    case '[' => true
+    case ']' => true
+    case '{' => true
+    case '}' => true
+    case '<' => true
+    case '+' => true
+    case '*' => true
+    case '/' => true
+    case _ => false
+  }
+  
+  private def isDoubledChar(c1: Char, c2: Char): Boolean = c1 == c2
 
 
   /** Reads the contents of a file, caching two characters at a time.
@@ -82,17 +142,89 @@ object Lexer extends Pipeline[File, Iterator[Token]] {
         while(currentChar != '\n' && currentChar != '\r' && currentChar != EndOfFile) consume()
         nextToken()
       } else if (currentChar == '/' && nextChar == '*') {
-        ??? // TODO handle multiline comments. Remember to fail for unclosed comments!
+        //The position at the beginning of the current comment to report in case of failing.
+        val posOfComment = currentPos 
+        consume(2)
+        while(!(currentChar == '*' && nextChar == '/') && (currentChar != EndOfFile)){
+          if(currentChar == EndOfFile || nextChar == EndOfFile){
+            error("Missing */ to end this bloc comment.", posOfComment)
+          }
+          consume()
+        }
+        nextToken(
+            )
       } else {
         readToken()
       }
     }
-
+    
+    lazy val listOfDigits: List[Char] = List('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
     /** Reads the next token from the stream. */
+    //@scala.annotation.tailrec
     def readToken(): Token = {
       // The position at the beginning of the token.
-      val tokenPos = currentPos
-      ??? // TODO
+      val tokenPos: Positioned = currentPos
+      
+      //first testing the case of a StringLiteral and wether it's closed.
+      if(currentChar == '"'){
+        consume() //to avoid putting the " in the String.
+        var str: String = ""
+        while(currentChar != '"' && currentChar != EndOfFile){
+           str += currentChar
+           consume()
+        }
+        if(currentChar == EndOfFile){
+          error("String Literal not closed", tokenPos)
+        }
+        consume() // for currentChar not to be == '"' after that.
+        STRINGLIT(str).setPos(tokenPos)
+      }
+      else if(isDoubledChar(currentChar, nextChar) && isASpecialChar(currentChar)){
+        
+        //testing the case of token such as "&&", "||" and "=="
+        val s: String = currentChar.toString() + nextChar.toString()
+        consume(2)
+        tokenAfterKW(s, tokenPos)
+        
+      }
+      else if(isASpecialChar(currentChar)){
+        
+        //case when we encoutered a special character (
+        val previousChar: String = currentChar.toString()
+        consume()
+        tokenAfterKW(previousChar, tokenPos) 
+        
+      }
+      else{
+        /*now trying to iterate to find a token that is either a keyword, 
+         * an identifier, or a IntLitteral.
+         */
+        var strBuild: String = ""
+        while(!Character.isWhitespace(currentChar) && !isASpecialChar(currentChar) && currentChar != EndOfFile){
+          strBuild += currentChar
+          consume()
+        }
+        val tokToTest: String = strBuild
+        keywords(tokToTest) match {
+          case Some(token) => token.setPos(tokenPos)
+          case None => {
+            //now we need to test wether it's and identifier, or a Intliteral
+            if(tokToTest != "" && tokToTest.forall { c => listOfDigits.contains(c)}){
+              INTLIT(tokToTest.toInt)
+            }else{
+              ID(tokToTest)
+            }
+          }
+        }
+      }
+    }
+    
+    //just for modularity.
+    def tokenAfterKW(s: String, tokenPos: Positioned): Token = {
+      keywords(s) match {
+          case Some(token) => token.setPos(tokenPos)
+          case None => BAD().setPos(tokenPos)
+      }
     }
 
     new Iterator[Token] {
