ef3d513fa80fb536fa7021b7a3241bacbe63ae3c
cedricviaccoz <cedric.viaccoz@gmail.com>
2016-10-16 15:46:06 +0200

    Tried to make a LL(1)

--- src/main/scala/toolc/ast/Parser.scala
+++ src/main/scala/toolc/ast/Parser.scala
@@ -14,8 +14,9 @@ import GrammarDSL._
 object Parser extends Pipeline[Iterator[Token], Program] {
 
   val toolGrammar = Grammar('Program, List[Rules[Token]](
-    'Program ::= 'MainObject ~ 'ClassDecls ~ EOF(),
-    'MainObject ::= PROGRAM() ~ 'Identifier ~ LBRACE() ~ 'Stmts ~ RBRACE(),
+    'Program ::= 'MainObject ~ 'ClassDecls ~ EOF(), 
+    // FIRST = {PROGRAM()}
+    'MainObject ::= PROGRAM() ~ 'Identifier ~ LBRACE() ~ 'Stmts ~ RBRACE(), //FIRST = {PROGRAM()}
     'Stmts ::= 'Statement ~ 'Stmts | epsilon(),
     'ClassDecls ::= 'ClassDeclaration ~ 'ClassDecls | epsilon(),
     'ClassDeclaration ::= CLASS() ~ 'Identifier ~ 'OptExtends ~ 'ClassBody,
@@ -38,10 +39,11 @@ object Parser extends Pipeline[Iterator[Token], Program] {
       | PRINTLN() ~ LPAREN() ~ 'Expression ~ RPAREN() ~ SEMICOLON()
       | 'Identifier ~ 'IdStat
       | DO() ~ LPAREN() ~ 'Expression ~ RPAREN() ~ SEMICOLON(),
+    // FIRST = {LBRACE, WHILE, PRINTLN, IDSENT, DO}
     'IdStat ::= EQSIGN() ~ 'Expression ~ SEMICOLON()
-      | LBRACKET() ~ 'Expression ~ RBRACKET() ~ EQSIGN() ~ 'Expression ~ SEMICOLON(),
+      | LBRACKET() ~ 'Expression ~ RBRACKET() ~ EQSIGN() ~ 'Expression ~ SEMICOLON(), //FIRST = {EQSIGN, LBRACKET}
     'ElseOpt ::= ELSE() ~ 'Statement | epsilon(),
-    'Expression ::= 'Expression ~ 'Op ~ 'Expression
+    'Expression ::= 'Expression ~ 'Op ~ 'Expression // LEFT RECURSION HERE !
       | 'Expression ~ LBRACKET() ~ 'Expression ~ RBRACKET()
       | 'Expression ~ DOT() ~ LENGTH()
       | 'Expression ~ DOT() ~ 'Identifier ~ LPAREN() ~ 'Args ~ RPAREN()
@@ -53,53 +55,135 @@ object Parser extends Pipeline[Iterator[Token], Program] {
       | LPAREN() ~ 'Expression ~ RPAREN(),
     'Args ::= epsilon() | 'Expression ~ 'ExprList,
     'ExprList ::= epsilon() | COMMA() ~ 'Expression ~ 'ExprList,
+    //FIRST = {e, COMMA()}
     'Op ::= AND() | OR() | EQUALS() | LESSTHAN() | PLUS() | MINUS() | TIMES() | DIV(),
     'Identifier ::= IDSENT
   ))
 
   // TODO: Transform this to an LL(1) grammar
   val ll1Grammar = Grammar('Program, List[Rules[Token]](
+      //no left-recursion anymore
+      //there seems to be nothing to left-fatorize
+      // the first rule of LL(1) (A -> a|b, First(a) (intersection) First(b) = 0
     'Program ::= 'MainObject ~ 'ClassDecls ~ EOF(),
+      //FIRST = program
+      //FOLLOW = 
     'MainObject ::= PROGRAM() ~ 'Identifier ~ LBRACE() ~ 'Stmts ~ RBRACE(),
+      //FIRST = program
+      //FOLLOW = class
     'Stmts ::= 'Statement ~ 'Stmts | epsilon(),
+      //FIRST = e if { while println IDSENT DO
+      //FOLLOW = }
+      //NULLABLE
     'ClassDecls ::= 'ClassDeclaration ~ 'ClassDecls | epsilon(),
+      //FIRST = e class
+      //FOLLOW = EOF 
+      //NULLABLE
     'ClassDeclaration ::= CLASS() ~ 'Identifier ~ 'OptExtends ~ 'ClassBody,
+      //FIRST = class
+      //FOLLOW = class EOF
     'OptExtends ::= epsilon() | EXTENDS() ~ 'Identifier,
+      //FIRST = e extends
+      //FOLLOW = {
+      //NULLABLE
     'ClassBody ::= LBRACE() ~ 'VarDecs ~ 'MethodDecs ~ RBRACE(),
+      //FIRST = {
+      //FOLLOW = 
     'VarDecs ::= 'VarDeclaration ~ 'VarDecs | epsilon(),
+      //FIRST = e var
+      //FOLLOW = 
+      //NULLABLE
     'VarDeclaration ::= VAR() ~ 'Param ~ SEMICOLON(),
+      //FIRST = var
+      //FOLLOW = 
     'MethodDecs ::= 'MethodDeclaration ~ 'MethodDecs | epsilon(),
+      //FIRST = e def
+      //FOLLOW = 
+      //NULLABLE
     'MethodDeclaration ::= DEF() ~ 'Identifier ~ LPAREN() ~ 'Params ~ RPAREN() ~ COLON() ~ 'Type ~ EQSIGN() ~ LBRACE() ~ 'VarDecs ~ 'Stmts ~ RETURN() ~ 'Expression ~ SEMICOLON() ~ RBRACE(),
+      //FIRST = def
+      //FOLLOW = 
     'Params ::= epsilon() | 'Param ~ 'ParamList,
+      //FIRST = e IDSENT
+      //FOLLOW = 
+      //NULLABLE
     'ParamList ::= epsilon() | COMMA() ~ 'Param ~ 'ParamList,
+      //FIRST = e ,
+      //FOLLOW =
+      //NULLABLE
     'Param ::= 'Identifier ~ COLON() ~ 'Type,
-    'Type ::= INT() ~ LBRACKET() ~ RBRACKET() | BOOLEAN() | INT() | STRING() | 'Identifier,
+      //FIRST = IDSENT
+      //FOLLOW = 
+    'Type ::= INT() ~ 'ArrayDeclaration
+      | BOOLEAN()  
+      | STRING() 
+      | 'Identifier,
+      //FIRST = Int Boolean String IDSENT
+      //FOLLOW = 
+    'ArrayDeclaration ::= epsilon()| LBRACKET() ~ RBRACKET(),
+      //FIRST = e [ 
+      //FOLLOW = 
+      //NULLABLE
     'Statement ::= IF() ~ LPAREN() ~ 'Expression ~ RPAREN() ~ 'MatchedIf ~ 'ElseOpt
       | 'SimpleStat,
+      //FIRST = if { while println IDSENT DO
+      //FOLLOW = 
     'MatchedIf ::= IF() ~ LPAREN() ~ 'Expression ~ RPAREN() ~ 'MatchedIf ~ ELSE() ~ 'MatchedIf
       | 'SimpleStat,
-    'SimpleStat ::= LBRACE() ~ 'Stmts ~ RBRACE()
+      // FIRST = if { while println IDSENT DO
+      //FOLLOW = 
+    'SimpleStat ::= LBRACE() ~ 'Stmt  ~ RBRACE()
       | WHILE() ~ LPAREN() ~ 'Expression ~ RPAREN() ~ 'MatchedIf
       | PRINTLN() ~ LPAREN() ~ 'Expression ~ RPAREN() ~ SEMICOLON()
       | 'Identifier ~ 'IdStat
       | DO() ~ LPAREN() ~ 'Expression ~ RPAREN() ~ SEMICOLON(),
+      //FIRST = { while println IDSENT DO
+      //FOLLOW = 
     'IdStat ::= EQSIGN() ~ 'Expression ~ SEMICOLON()
       | LBRACKET() ~ 'Expression ~ RBRACKET() ~ EQSIGN() ~ 'Expression ~ SEMICOLON(),
+      //FIRST = = [
+      //FOLLOW = 
     'ElseOpt ::= ELSE() ~ 'Statement | epsilon(),
-    'Expression ::= 'Expression ~ 'Op ~ 'Expression
-      | 'Expression ~ LBRACKET() ~ 'Expression ~ RBRACKET()
-      | 'Expression ~ DOT() ~ LENGTH()
-      | 'Expression ~ DOT() ~ 'Identifier ~ LPAREN() ~ 'Args ~ RPAREN()
-      | INTLITSENT | STRINGLITSENT
-      | TRUE() | FALSE() | 'Identifier | THIS()
-      | NEW() ~ INT() ~ LBRACKET() ~ 'Expression ~ RBRACKET()
-      | NEW() ~ 'Identifier ~ LPAREN() ~ RPAREN()
-      | BANG() ~ 'Expression
-      | LPAREN() ~ 'Expression ~ RPAREN(),
+      //FIRST = else
+      //FOLLOW = 
+      //NULLABLE
+    'Expression ::= INTLITSENT ~ 'ExpressionAlt
+      | STRINGLITSENT ~ 'ExpressionAlt
+      | TRUE() ~ 'ExpressionAlt
+      | FALSE() ~ 'ExpressionAlt
+      | 'Identifier ~ 'ExpressionAlt
+      | THIS() ~ 'ExpressionAlt
+      | NEW() ~ 'newExpr ~ 'ExpressionAlt
+      | BANG() ~ 'Expression ~ 'ExpressionAlt
+      | LPAREN() ~ 'Expression ~ RPAREN() ~ 'ExpressionAlt,
+      //FIRST = INTLIT STRINGLIT true false this new ! (
+      //FOLLOW = 
+    'ExpressionAlt ::= 'Op ~ 'Expression ~ 'ExpressionAlt
+      | LBRACKET() ~ 'Expression ~ RBRACKET() ~ 'ExpressionAlt
+      | DOT() ~ LENGTH() ~ 'ExpressionAlt
+      | DOT() ~ 'Identifier ~ LPAREN() ~ 'Args ~ RPAREN() ~ 'ExpressionAlt 
+      | epsilon(),
+      //FIRST = e { . && || == < + - * /
+      //FOLLOW = 
+      //NULLABLE
+    'newExpre ::= INT() ~ LBRACKET() ~ 'Expression ~ RBRACKET()
+      | 'Identifier ~ LPAREN() ~ RPAREN(),
+      //FIRST = Int IDSENT
+      //FOLLOW = 
     'Args ::= epsilon() | 'Expression ~ 'ExprList,
+      //FIRST = e INTLIT STRINGLIT true false this new ! (
+      //FOLLOW = 
+      //NULLABLE
     'ExprList ::= epsilon() | COMMA() ~ 'Expression ~ 'ExprList,
+      //FIRST = e ,
+      //FOLLOW = 
+      //NULLABLE
     'Op ::= AND() | OR() | EQUALS() | LESSTHAN() | PLUS() | MINUS() | TIMES() | DIV(),
+      // FIRST = && || == < + - * /
+      //FOLLOW = 
     'Identifier ::= IDSENT
+      //FIRST = IDSENT
+      //FOLLOW = { extends
   ))
 
   def run(ctx: Context)(tokens: Iterator[Token]): Program = {
