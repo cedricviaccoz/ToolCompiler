fb670eb6d07124149325807a30953b766fb167dd
cedricviaccoz <cedric.viaccoz@gmail.com>
2016-11-13 20:27:25 +0100

    First full implementation of Lab04

--- src/main/scala/toolc/analyzer/NameAnalysis.scala
+++ src/main/scala/toolc/analyzer/NameAnalysis.scala
@@ -22,12 +22,12 @@ object NameAnalysis extends Pipeline[Program, Program] {
       for (c <- prog.classes) {
         val className = c.id.value
         if (global.classes.contains(className)) {
-          ctx.reporter.error(s"Class ${c.id.value} is already defined at position ${global.classes.get(className).get.position}")
+          error(s"Class ${c.id.value} is already defined at position ${global.classes.get(className).get.position}")
         } else if (c.id == "Object") {
-          ctx.reporter.error(s"the class at position ${c.position} cannot bear the name 'Object' !")
+          error(s"the class at position ${c.position} cannot bear the name 'Object' !")
 
         } else {
-          global.classes + ((className, new ClassSymbol(className).setPos(c)))
+          global.classes += ((className, new ClassSymbol(className).setPos(c)))
         }
       }
 
@@ -86,35 +86,55 @@ object NameAnalysis extends Pipeline[Program, Program] {
             collectInClass(parentClDecl)
 
             //adding the parent's inherited fields and methods
-            correspClassSym.members ++ correspClassSym.parent.get.members
-            correspClassSym.methods ++ correspClassSym.parent.get.methods
-            
+            correspClassSym.members ++= correspClassSym.parent.get.members
+            correspClassSym.methods ++= correspClassSym.parent.get.methods
+
             //and analyzing/adding this class own's members and methods
             analyseMembers(correspClassSym, c.vars)
             for (method <- c.methods) {
-              ???
+              val lookupMeth = correspClassSym.lookupMethod(method.id.value)
+              if (lookupMeth isDefined) {
+                //duplicate method case
+                if (lookupMeth.get.classSymbol == correspClassSym) {
+                  error(s"Method at position ${method.position} is defined twice, first time at ${lookupMeth.get.position}")
+                } else {
+
+                  //we are in the overriden method state here.
+                  val methSym = new MethodSymbol(method.id.value, correspClassSym).setPos(method)
+                  methSym.overridden = lookupMeth
+
+                  initMethSymbol(methSym, method)
+                  if (methSym.argList.size == lookupMeth.get.argList.size) {
+                    correspClassSym.methods += ((method.id.value, methSym))
+                  } else {
+                    //the overriden method doesn't have the same number of parameters as its parent class, need to report the error 
+                    error(s"Overrident method at ${methSym.position} don't have the same number of parameter as the method it overrides at ${lookupMeth.get.position}")
+                  }
+
+                }
+              } else {
+
+                //method is not overriden, treat it normally.
+                val methSym = new MethodSymbol(method.id.value, correspClassSym).setPos(method)
+                initMethSymbol(methSym, method)
+                correspClassSym.methods += ((method.id.value, methSym))
+
+              }
             }
 
           } else {
             //case of no inherited class
             analyseMembers(correspClassSym, c.vars)
-            
+
             for (method <- c.methods) {
               val lookupMeth = correspClassSym.lookupMethod(method.id.value)
-              if(lookupMeth isDefined){
-                ctx.reporter.error(s"Method at position ${method.position} defined twice, first time at ${lookupMeth.get.position}")
-              }else{
-                
+              if (lookupMeth isDefined) {
+                error(s"Method at position ${method.position} is defined twice, first time at ${lookupMeth.get.position}")
+              } else {
+
                 val methSym = new MethodSymbol(method.id.value, correspClassSym).setPos(method)
-                for(args <- method.args){
-                  if(methSym.params.contains(args.id.value)){
-                    ctx.reporter.error(s"method's arguments are duplicated, first occurence found at ${methSym.params(args.id.value).position}")
-                  }else{
-                    val argSymbol = new VariableSymbol(args.id.value).setPos(args)
-                    methSym.params + ((args.id.value, argSymbol)) 
-                    methSym.argList.::(argSymbol)
-                  }
-                } 
+                initMethSymbol(methSym, method)
+                correspClassSym.methods += ((method.id.value, methSym))
               }
             }
           }
@@ -122,12 +142,39 @@ object NameAnalysis extends Pipeline[Program, Program] {
         }
       }
 
-      def analyseMembers(currClass: ClassSymbol, vars: List[VarDecl]) = for (variable <- vars) {
+      //helper method which takes care of "symbolise" the params and members of a method.
+      def initMethSymbol(methSym: MethodSymbol, methodDecl: MethodDecl) = {
+        for (args <- methodDecl.args) {
+          if (methSym.params.contains(args.id.value)) {
+            error(s"method's arguments are duplicated, first occurence found at ${methSym.params(args.id.value).position}")
+          } else {
+            val argSymbol = new VariableSymbol(args.id.value).setPos(args)
+            methSym.params += ((args.id.value, argSymbol))
+            methSym.argList.::(argSymbol)
+          }
+        }
+        analysMethodVars(methSym, methodDecl.vars)
+      }
+      //makes symbol of class members and add them to the corresponding classSymbol
+      def analyseMembers(currClass: ClassSymbol, vars: List[VarDecl]): Unit = for (variable <- vars) {
         val varName = variable.id.value
         if (currClass.members.contains(varName)) {
-          ctx.reporter.error(s"variable at ${variable.position} is already defined at ${currClass.members.get(varName).get.position}")
+          error(s"variable at ${variable.position} is already defined at ${currClass.members.get(varName).get.position}")
         } else {
-          currClass.members + ((varName, new VariableSymbol(varName).setPos(variable)))
+          currClass.members += ((varName, new VariableSymbol(varName).setPos(variable)))
+        }
+      }
+
+      //makes symbol of method variables and add them to the corresponding MethodSymbol
+      def analysMethodVars(currMethod: MethodSymbol, vars: List[VarDecl]): Unit = {
+        for (member <- vars) {
+          if (currMethod.members.contains(member.id.value)) {
+            error(s"method's variable at ${member.position} is already declared at ${currMethod.members(member.id.value).position}")
+          } else if (currMethod.params.contains(member.id.value)) {
+            error(s"method variable at ${member.position} try to override method parameter at ${currMethod.params(member.id.value).position}")
+          } else {
+            currMethod.members += ((member.id.value, new VariableSymbol(member.id.value).setPos(member)))
+          }
         }
       }
 
@@ -137,24 +184,52 @@ object NameAnalysis extends Pipeline[Program, Program] {
     def setPSymbols(prog: Program, gs: GlobalScope): Unit = {
       // TODO: Traverse within each definition of the program
       //       and attach symbols to Identifiers and "this"
-      ???
+      prog.classes foreach (setCSymbols(_, gs))
+      prog.main.stats foreach (setSSymbols(_)(gs, None))
     }
 
     def setCSymbols(klass: ClassDecl, gs: GlobalScope): Unit = {
       val classSym = gs.lookupClass(klass.id.value).get
       for (varDecl <- klass.vars) {
-        setTypeSymbol(varDecl.tpe, gs)
+        /*
+         * old Version was : setTypeSymbol(varDecl.tpe, gs)
+         */
+        setTypeSymbol(varDecl, classSym, gs)
       }
 
       klass.methods.foreach(setMSymbols(_, gs, classSym))
     }
 
     def setMSymbols(meth: MethodDecl, gs: GlobalScope, cs: ClassSymbol): Unit = {
-      ??? // TODO
+      val MethodSym = cs.lookupMethod(meth.id.value).get
+      meth.setSymbol(MethodSym)
+      meth.id.setSymbol(MethodSym)
+      meth.args foreach (p => setISymbol(p.id)(Some(MethodSym)))
+      meth.vars foreach (p => setISymbol(p.id)(Some(MethodSym)))
     }
 
-    def setSSymbols(stat: StatTree)(implicit gs: GlobalScope, ms: Option[MethodSymbol]): Unit = {
-      ??? // TODO
+    def setSSymbols(stat: StatTree)(implicit gs: GlobalScope, ms: Option[MethodSymbol]): Unit = stat match {
+      case Block(stats: List[StatTree]) => stats foreach (setSSymbols(_))
+      case If(expr: ExprTree, thn: StatTree, els: Option[StatTree]) =>
+        setESymbols(expr)
+        setSSymbols(thn)
+        if (els isDefined) setSSymbols(els.get)
+      case While(expr: ExprTree, stat: StatTree) =>
+        setESymbols(expr)
+        setSSymbols(stat)
+      case Println(expr: ExprTree) => setESymbols(expr)
+      case Assign(id: Identifier, expr: ExprTree) =>
+        if (ms isDefined) {
+          setISymbol(id)
+        } else error("Assignment made on the main object at " + id.position)
+        setESymbols(expr)
+      case ArrayAssign(id: Identifier, index: ExprTree, expr: ExprTree) =>
+        if (ms isDefined) {
+          setISymbol(id)
+        } else error("Assignment made on the main object at " + id.position)
+        setESymbols(index)
+        setESymbols(expr)
+      case DoExpr(e: ExprTree) => setESymbols(e)
     }
 
     def setISymbol(id: Identifier)(implicit ms: Option[MethodSymbol]) = {
@@ -167,12 +242,48 @@ object NameAnalysis extends Pipeline[Program, Program] {
       }
     }
 
-    def setESymbols(expr: ExprTree)(implicit gs: GlobalScope, ms: Option[MethodSymbol]): Unit = {
-      ??? // TODO
+    def setESymbols(expr: ExprTree)(implicit gs: GlobalScope, ms: Option[MethodSymbol]): Unit = expr match {
+      case And(lhs: ExprTree, rhs: ExprTree) =>
+        setESymbols(lhs); setESymbols(rhs)
+      case Or(lhs: ExprTree, rhs: ExprTree) =>
+        setESymbols(lhs); setESymbols(rhs)
+      case Plus(lhs: ExprTree, rhs: ExprTree) =>
+        setESymbols(lhs); setESymbols(rhs)
+      case Minus(lhs: ExprTree, rhs: ExprTree) =>
+        setESymbols(lhs); setESymbols(rhs)
+      case Times(lhs: ExprTree, rhs: ExprTree) =>
+        setESymbols(lhs); setESymbols(rhs)
+      case Div(lhs: ExprTree, rhs: ExprTree) =>
+        setESymbols(lhs); setESymbols(rhs)
+      case LessThan(lhs: ExprTree, rhs: ExprTree) =>
+        setESymbols(lhs); setESymbols(rhs)
+      case Equals(lhs: ExprTree, rhs: ExprTree) =>
+        setESymbols(lhs); setESymbols(rhs)
+      case ArrayRead(arr: ExprTree, index: ExprTree) =>
+        setESymbols(arr); setESymbols(index)
+      case ArrayLength(arr: ExprTree) => setESymbols(arr)
+      case MethodCall(obj: ExprTree, meth: Identifier, args: List[ExprTree]) =>
+        setESymbols(obj); args foreach (setESymbols(_))
+      case Variable(id: Identifier) => setISymbol(id)
+      case th: This =>
+        if (ms isDefined) th.setSymbol(ms.get.classSymbol)
+        else error(s"Cannot call reference to 'this' at${expr.position} on the main object")
+      case NewIntArray(size: ExprTree) => setESymbols(size)
+      case New(tpe: Identifier) =>
+        val newClass = gs.lookupClass(tpe.value)
+        if (newClass isDefined) tpe.setSymbol(newClass get)
+        else error(s"The class which is constructed at ${tpe.position} has not been declared.")
+      case Not(expr: ExprTree) => setESymbols(expr)
+      case _                   =>
     }
 
-    def setTypeSymbol(tpe: TypeTree, gs: GlobalScope): Unit = {
-      ??? // TODO
+    /*
+     * Old version was :
+     * def setTypeSymbol(tpe: TypeTree, gs: GlobalScope): Unit = {???}
+     */
+    
+    def setTypeSymbol(tpe: VarDecl, cs: ClassSymbol, gs: GlobalScope): Unit = {
+      val varSym = cs.lookupVar(tpe.id.value)
     }
 
     val gs = collectSymbols(prog)
