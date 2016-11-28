1bd50e0729aed4bb19dd5623f61a0df81feb55e9
Manos Koukoutos <emmanouil.koukoutos@epfl.ch>
2016-11-28 15:39:28 +0100

    Stubs for Lab06: Code generation

--- src/main/scala/toolc/Main.scala
+++ src/main/scala/toolc/Main.scala
@@ -1,30 +1,54 @@
 package toolc
 
 import utils._
+import java.io.File
+
 import lexer._
 import ast._
+import eval._
 import analyzer._
-import java.io.File
+import code._
 
 object Main {
 
   def processOptions(args: Array[String]): Context = {
-    val (opts, files) = args.toSeq.partition(_.startsWith("--"))
     val reporter = new Reporter()
+    var files: List[File] = Nil
+    var outDir: Option[File] = None
+
+    def rec(args: List[String]): Unit = args match {
+      case "-d" :: dir :: xs =>
+        outDir = Some(new File(dir))
+        rec(xs)
+
+      case f :: xs =>
+        files  ::= new File(f)
+        rec(xs)
+
+      case _ =>
+    }
+
+    rec(args.toList)
 
     if (files.size != 1) {
       reporter.fatal("Exactly one file expected, "+files.size+" file(s) given.")
     }
 
-    Context(reporter = reporter, files = new File(files.head) :: Nil)
+    Context(reporter = reporter, files = files, outDir = outDir)
   }
 
 
   def main(args: Array[String]) {
     val ctx = processOptions(args)
-    val pipeline = Lexer andThen Parser andThen NameAnalysis andThen TypeChecking
-    val ast = pipeline.run(ctx)(ctx.files.head)
-    ctx.reporter.terminateIfErrors()
-  }
 
+    val pipeline = Lexer andThen
+                   Parser andThen
+                   NameAnalysis andThen
+                   TypeChecking andThen
+                   CodeGeneration
+
+    pipeline.run(ctx)(ctx.files.head)
+
+    ctx.reporter.terminateIfErrors
+  }
 }
